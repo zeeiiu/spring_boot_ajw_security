08f1308adb10cbb645f1bbf68b1c5932b485235f
zeeiiu <dkswldn1004@naver.com>
2023-04-17 12:00:44 +0900

    first commit

---
+++ src/test/java/edu/personal/ex/SpringBootAjwSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.personal.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAjwSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
