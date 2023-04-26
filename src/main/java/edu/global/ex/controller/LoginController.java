bdd41067e83f3aaa595107e41c116aa34d0ee918
zeeiiu <dkswldn1004@naver.com>
2023-04-26 12:59:44 +0900

    트랜잭션 완료

--- src/main/java/edu/global/ex/controller/LoginController.java
+++ src/main/java/edu/global/ex/controller/LoginController.java
@@ -2,21 +2,33 @@ package edu.global.ex.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.global.ex.service.UserService;
+import edu.global.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
 @Controller
 public class LoginController {
-
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@Autowired
+	   private PasswordEncoder encoder;
+	  
 	@GetMapping("/login")
 	public String login() {
 		log.info("login()..");
@@ -41,6 +53,29 @@ public class LoginController {
 	      System.out.println("Principal 유저 아이디:" + principal.getName() );
 	        
 	        return "redirect:/";
+	   }
+	   
+	   //restful 방식. get 방식으로 했으면 ? 달렸어야함
+	   @GetMapping("/addUser/{id}/{pw}") // url을 파라미터화 시키는 것. @PathVariable을 통해서 파라미터화 시킨 것을 넘겨줌.
+	   public String addUser(@PathVariable String id, @PathVariable String pw) throws Exception {
+		   
+		 System.out.println(id + ":" + pw);
+		 
+		 UserVO user = new UserVO();
+	      user.setEnabled(1);
+	      user.setUsername(id);
+	      user.setPassword(encoder.encode(pw.toString().trim()));
+	      
+	      System.out.println(user);
+
+	     // userService.addUser(user);
+         // userService.addUser2(user);
+	     // userService.addUser3(user);
+	     // userService.addUser4(user);
+	     //  userService.addUser5(user);
+	       userService.addUser6(user);
+		 
+		 return "redirect:/";
 	   }
 
 }
--- src/main/java/edu/global/ex/service/UserService.java
+++ src/main/java/edu/global/ex/service/UserService.java
@@ -2,6 +2,7 @@ package edu.global.ex.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.global.ex.mapper.UserMapper;
 import edu.global.ex.vo.UserVO;
@@ -9,16 +10,90 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service // 스프링으로 하여금 mybatis 인터페이스 인걸 알려줌
-public class UserService { 
-	
-	
+public class UserService {
+
 	@Autowired
 	private UserMapper userMapper;
-	
-	public int addUser(UserVO user) {
+
+	// 1.정상 동작 코드 => User를 insert를 넣은후 권한 까지 insert 해야 됨 => 2개가 정상적인 동작이 되어야 됨(트랜잭션 단위)
+	// 2. 2개가 정상적인 동작이 되어야 됨 (트랜잭션 단위)
+	// 에는 들가 있으나 select * from authorities 권한에 안들어 가있음
+	public void addUser(UserVO user) {
 		log.info("addUser()..");
-		return userMapper.insertUser(user);
+
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		userMapper.insertAuthorities(user); // 권한 설정
+
 	}
 	
-}
+	
+	
+	public void addUser2 (UserVO user) {
+		log.info("addUser()..");
 
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		
+		
+		//일부러 에러냄
+		user = null;
+	
+		userMapper.insertAuthorities(user); // 권한 설정
+	}
+	
+
+	// 3. 아래처럼 @Transactional을 붙임 // 롤백이 되어 아무런 데이터가 들어가지 않음. 윗 소스는 @Transactional안붙인거
+	@Transactional
+	public void addUser3 (UserVO user) {
+		log.info("addUser()..");
+		
+		userMapper.insertUser(user); // User를 insert를 넣은후
+		
+		
+		//일부러 에러냄
+		user = null;
+		userMapper.insertAuthorities(user); // 권한 설정
+		
+	}
+	
+	   // 4.checked Exception을 강제로 시켜 봄 //롤백 되지 않음
+	   @Transactional
+	   public void addUser4(UserVO user) throws Exception {
+	      log.info("addUser()..");
+
+	      userMapper.insertUser(user); // User를 insert를 넣은후
+	      userMapper.insertAuthorities(user);
+
+	      // throw Checked Exception
+	      throw new Exception("Exception (Checked Exception)"); // 테스트를 위해 강제로 에러냄
+	   }
+	   
+
+	   // 5. unchecked Exception을 강제로 시켜 봄 //롤백 됨
+	   @Transactional
+	   public void addUser5(UserVO user) throws Exception {
+		   log.info("addUser()..");
+		   
+		   userMapper.insertUser(user); // User를 insert를 넣은후
+		   userMapper.insertAuthorities(user);
+		   
+		   // throw Checked Exception
+		   throw new RuntimeException("RuntimeException (Unchecked Exception)"); // 테스트를 위해 강제로 에러냄
+	   }
+	
+
+	   // 6. rollbackFor 옵션을 줌 //롤백 됨
+	   @Transactional (rollbackFor = Exception.class) // 체크든 언체크든 롤백시키겠다. Exception은 최고 부모
+	   public void addUser6(UserVO user) throws Exception {
+		   log.info("addUser()..");
+		   
+		   userMapper.insertUser(user); // User를 insert를 넣은후
+		   userMapper.insertAuthorities(user);
+		   
+		   // throw Checked Exception
+		   throw new RuntimeException("RuntimeException (Unchecked Exception)"); // 테스트를 위해 강제로 에러냄
+	   }
+	   
+	
+	
+	
+}
